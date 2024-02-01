230660972843ef0ace17da1423628398c4f18082
Kenneth Grina <grina.kenneth@gmail.com>
2024-01-31 20:41:16 -0600

    Q1-3

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,16 +15,16 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  return;
+  return `Hello, ${name}!`;
 }
 // Remember to call the function to test
-
-
+console.log(helloName('Bender'));
 // 3. Function to add two numbers together & return the result
-function addNumbers(firstNumber) {
+function addNumbers(firstNumber, secondNumber) {
+  return firstNumber + secondNumber  
   // return firstNumber + secondNumber;
 }
-
+console.log(addNumbers(56,984675));
 
 // 4. Function to multiply three numbers & return the result
 function multiplyThree() {
